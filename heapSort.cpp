30c4f96b9a973bdcc5a0d069e0954408b8dcb961
jasonzhou1997 <60407587+jasonzhou1997@users.noreply.github.com>
2022-05-15 09:37:42 -0400

    Updated version to match 0 base index

--- heapSort.cpp
+++ heapSort.cpp
@@ -3,27 +3,34 @@
 #include <vector>
 using namespace std;
 
-void fixDown(vector<int> &heap, int start, int end) {
+void fixDown(vector<int> &heap, int heapsize, int k) {
 
-    int dad = start;
-    int son = dad * 2 + 1;
-    while (son <= end) {
-        if (son + 1 <= end && heap[son] < heap[son + 1])
-            son++;
-        if (heap[dad] > heap[son]) 
+    while (2 * k + 1 <= heapsize) {
+        int j = k * 2 + 1;
+        if (j < heapsize && heap[j] < heap[j + 1])
+        {
+            ++j;
+        }
+        if (heap[k] > heap[j]) 
             return;
         else { 
-            swap(heap[dad], heap[son]);
-            dad = son;
-            son = dad * 2 + 1;
+            swap(heap[k], heap[j]);
+            k = j;
         }
     }
 }
+
 void heapify(vector<int> &heap, int len)
 {
     for (int i = len / 2 - 1; i >= 0; i--)
     {
-        fixDown(heap, i, len - 1);
+        fixDown(heap, len - 1, i);
+        for (int i = 0; i < len; i++)
+        {
+            cout << heap[i] << ' ';
+        }
+        cout << "\n";
+
     }
 
 }
@@ -31,10 +38,9 @@ void heapify(vector<int> &heap, int len)
 void heap_sort(vector<int> &heap, int len) {
 
     heapify(heap, len);
-
     for (int i = len - 1; i > 0; i--) {
         swap(heap[0], heap[i]);
-        fixDown(heap, 0, i - 1);
+        fixDown(heap, i - 1, 0);
     }
 }
 
@@ -42,8 +48,10 @@ int main() {
     vector<int> heap = {4, 2, 5, 1, 7, 9, 14, 18, 81};
     int len = heap.size();
     heap_sort(heap, len);
+    cout << "\n";
     for (int i = 0; i < len; i++)
         cout << heap[i] << ' ';
-    cout << endl;
+    cout << "\n";
+
     return 0;
 }
